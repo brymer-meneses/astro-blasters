0406bc027273549f7f33894c3b9b38dd580ed128
UnoRamos <150862634+UnoRamos@users.noreply.github.com>
2024-11-23 08:39:43 +0800

    Update SFX sound (#13)

--- assets/assets.go
+++ assets/assets.go
@@ -77,6 +77,22 @@ var ships []byte
 //go:embed SpaceShooterAssetPack/IU.png
 var iu []byte
 
+//go:embed sfx/laser.wav
+var LaserAudio []byte
+
+//go:embed sfx/start.wav
+var StartAudio []byte
+
+//go:embed sfx/background.wav
+var BackgroundAudio []byte
+
+func mustLoadAudioFromBytes(data []byte) []byte {
+	if len(data) == 0 {
+		panic("audio file is empty or not embedded correctly")
+	}
+	return data
+}
+
 func mustLoadSpriteFromBytes(data []byte, width, height int) Sprite {
 	image, _, err := ebitenutil.NewImageFromReader(bytes.NewReader(data))
 	if err != nil {
